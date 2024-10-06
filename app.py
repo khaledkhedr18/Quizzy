82cb3f19199c5e439f8385fd71f099f57da0eb91
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-10-07 01:24:42 +0300

    created showlinks.css which edits the static style of the header, created login.html and register.html which has the structure of the login and the register pages. edited the main app.py to route for all the new pages.

--- app.py
+++ app.py
@@ -8,5 +8,20 @@ def index():
     return render_template("home.html")
 
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    return render_template("home.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
